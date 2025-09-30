b7fd9de20eb4b578461fba1d44194389848152cd
Ian Isaacson <ian_isaacson@yahoo.com>
2025-09-29 22:48:01 -0700

    Complete two-pass, single-pointer solution.

--- removeNthLastNode.js
+++ removeNthLastNode.js
@@ -8,8 +8,37 @@
 import { ListNode } from "./ds_v1/LinkedList.js";
 
 function removeNthLastNode(head, n) {
-  // Replace this placeholder return statement with your code
+  let depth = findDepth(head);
+  let current = head;
+
+  // If we need to remove the head node
+  if (depth === n) {
+    head = head.next;
+  } else {
+    let targetIndex = depth - n;
+    current = head;
+    let index = 0;
+
+    while (current) {
+      if (index === targetIndex) {
+        current.next = current.next ? current.next.next : null;
+        break;
+      }
+      current = current.next;
+      index++;
+    }
+  }
+
   return head;
+}
+
+function findDepth(node) {
+  let depth = 0;
+  while (node) {
+    depth++;
+    node = node.next;
+  }
+  return depth;
 }
 
 export { removeNthLastNode };
