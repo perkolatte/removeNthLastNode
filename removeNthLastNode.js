5d983411cfc84aae5cd091d1ddfa08a84a08a008
Ian Isaacson <ian_isaacson@yahoo.com>
2025-09-29 22:22:35 -0700

    Add starter code

---
+++ removeNthLastNode.js
@@ -0,0 +1,15 @@
+// Definition for a Linked List node
+// class ListNode {
+//     constructor(val = 0, next = null) {
+//         this.val = val;
+//         this.next = next;
+//     }
+// }
+import { ListNode } from "./ds_v1/LinkedList.js";
+
+function removeNthLastNode(head, n) {
+  // Replace this placeholder return statement with your code
+  return head;
+}
+
+export { removeNthLastNode };
